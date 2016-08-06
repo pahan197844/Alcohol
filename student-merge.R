6bfd49e1052983044b1260d4333b5473fe72b3ab
pahan197844 <ddppvv@gmail.com>
2016-08-05 20:38:35 -0400

    Add files via upload

--- student-merge.R
+++ student-merge.R
@@ -1,22 +1,56 @@
-d1=read.table("student-mat.csv",sep=";",header=TRUE)
+require(foreign)
+require(nnet)
+require(ggplot2)
+require(reshape2)
+#load data
+
 d2=read.table("student-por.csv",sep=";",header=TRUE)
 
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
-str(d3)
-summary((d3))
+names (d2)
+str(d2)
+summary(d2)
+head(d2, 10)
+table(d2$Dalc  )
+table(d2$Walc  )
 
-table(d3$Dalc.y )
-table(d3$Walc.y )
-
-model1 = lm(Walc.x~ sex+ age+famsize+ activities.x+ higher.x +
-              +famrel.x + freetime.x, data = d3)
-summary (model1)
-SSE1 = sum(model1$residuals^2)
+model.days = lm(Dalc~ sex+ age+famsize+ activities+ higher +
+              +famrel + freetime, data = d2)
+summary (model.days)
+SSE1 = sum(model.days$residuals^2)
 SSE1
+model.weekend = lm(Walc~ sex+ age+famsize+ activities+ higher +
+              +famrel + freetime, data = d2)
 
-model2 = lm(Walc.x~ sex+ age+famsize+Pstatus+ Medu+Fedu + studytime.x +failures.x+ schoolsup.x+ activities.x+ higher.x +romantic.x
-           +            +famrel.x + freetime.x+ goout.x+goout.x, data = d3)
+
+model2 = lm(Dalc~ sex+ age+famsize+Pstatus+ Medu+Fedu + studytime +failures+ schoolsup+ activities+ higher +romantic
+                +famrel + freetime+goout, data = d2)
 summary (model2)
 SSE2 = sum(model2$residuals^2)
 SSE2
+
+library("nnet")
+test = multinom(Walc ~ sex+ age+famsize+Pstatus+ Medu+Fedu + studytime +failures+ schoolsup+ activities+ higher +romantic
+                 +famrel + freetime+goout, data = d2)
+summary(test)
+z <- summary(test)$coefficients/summary(test)$standard.errors
+z
+p <- (1 - pnorm(abs(z), 0, 1))*2
+p
+exp(coef(test))
+
+names (d2)
+test <- multinom(Dalc ~ ., data = d2)
+
+head(fitted(test))
+predicted=predict(test,type="probs")
+head(predicted)
+bpp=cbind(d2, predicted)
+names (bpp)
+by(bpp[,34:38], bpp$Walc, colMeans)
+bpp2 = melt (bpp,id.vars=c(names (d2)),value.name="probablity")
+
+head(bpp2)
+ library(ggplot2)
+
+ggplot(bpp2, aes(x = write, y = probablity, colour = Walc)) + facet_grid(variable ~ ., scales="free")
+
