4ea6b9e8b1d97080072481f014d30fb0f569329f
pahan197844 <ddppvv@gmail.com>
2016-08-24 15:37:30 -0400

    Update Capstone_Dudin.R

--- Capstone_Dudin.R
+++ Capstone_Dudin.R
@@ -1,1 +1,31 @@
-## Code for capstone project: The most important foreign languages for English-speaking job seekers in global development
+## Code for capstone project:  PREDICTING SCHOOL STUDENT ALCOHOL CONSUMPTION AND OVERALL PERFORMANCE
+
+#installing packages
+
+wants <- c("mlogit", "nnet", "VGAM","nnet","rpart.plot","caret","lift","nnet","ggplot2","reshape2","caTools","mlbench")
+has   <- wants %in% rownames(installed.packages())
+if(any(!has)) install.packages(wants[!has])
+
+#loading data
+setwd("C:/Users/111/Desktop/Alcohol-master")
+d1=read.table("student-mat.csv",sep=";",header=TRUE)
+d2=read.table("student-por.csv",sep=";",header=TRUE)
+#there are severalstudents that belong to both datasets . 
+#These students can be identified by searching for identical attributes
+#that characterize each student.
+
+df=rbind(d1, d2) 
+str(df)
+
+#creating the list of the attributes to search by
+name=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+#creating the uniqe index using mgcv
+library(mgcv)
+byname=subset(df,select=name)
+unique=uniquecombs(byname)
+uniqueIndex<-attr(unique,"index")
+d3=df[uniqueIndex,]
+
+
+
